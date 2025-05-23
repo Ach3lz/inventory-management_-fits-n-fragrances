5adb32a18833d13265603e05161439761c80be20
Achel <achelmichael80@gmail.com>
2025-05-23 17:40:45 +0000

    Fix deployment configuration

--- fits_and_fragrances_manager/wsgi.py
+++ fits_and_fragrances_manager/wsgi.py
@@ -1,6 +1,3 @@
-import dotenv
-dotenv.load_dotenv()
-
 """
 WSGI config for fits_and_fragrances_manager project.
 
@@ -11,8 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+
+# Try to load .env file if it exists
+try:
+    import dotenv
+    dotenv.load_dotenv()
+except:
+    pass
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fits_and_fragrances_manager.settings')
 
